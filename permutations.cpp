274e1a4b3161e36fd580e7d50ead49b7ee5adb02
Iruthik <iruthik.sankar@gmail.com>
2025-03-15 15:56:57 +0530

    added comments and cosmetics for perumatations.cpp

--- permutations.cpp
+++ permutations.cpp
@@ -1,8 +1,17 @@
+/*
+*Section : Arrays and Strings                           
+*Book    : Cracking The Coding Interview                 *Author  : Iruthik Sankar
+*Question2
+Given two strings ,write a method to decide if one is a permutation of the other
+ *
+ **/
+
 #include<iostream>
 #include<string>
 #include<algorithm>
 using namespace std;
 
+//Sorting Function
 string sortString(string s)
 {
 
@@ -12,7 +21,7 @@ string sortString(string s)
 
 bool permutations(string s1, string s2)
 {
-
+	//checking two strings have same length or not
 	if(s1.length() != s2.length())
 	{
 		return false;
